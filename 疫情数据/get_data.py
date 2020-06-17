e49c8885bcf74a5344e174995eacba9049e47aa7
xiaobei <258176484@qq.com>
2020-06-17 09:51:07 +0800

    数据采集

---
+++ 疫情数据/get_data.py
@@ -0,0 +1,45 @@
+import requests
+from lxml import etree
+import json
+import openpyxl
+
+url = 'https://voice.baidu.com/act/newpneumonia/newpneumonia'
+response = requests.get(url)
+# print(response.text)
+# 生成HTML对象
+html = etree.HTML(response.text)
+result = html.xpath('//script[@type="application/json"]/text()')
+result = result[0]
+# json.loads()方法可以将字符串转化为python数据类型
+result = json.loads(result)
+
+# 创建工作簿
+wb = openpyxl.Workbook()
+# 创建工作表
+ws = wb.active
+ws.title = '国内疫情'
+ws.append(['省份', '累计确诊', '死亡', '治愈', '现有确诊', '累计确诊增量', '死亡增量', '治愈增量', '现有确诊数量'])
+result_in = result['component'][0]['caseList']
+result_out = result['component'][0]['globalList']
+# print(result_out)
+for each in result_in:
+    temp_list = [each['area'], each['confirmed'], each['died'], each['crued'], each['curConfirm'], each['confirmedRelative'], each['diedRelative'], each['curedRelative'], each['curConfirmRelative']]
+    for i in range(len(temp_list)):
+        if temp_list[i] == '':
+            temp_list[i] = '0'
+    ws.append(temp_list)
+
+for each in result_out:
+    sheet_title = each['area']
+    # 创建新的工作表
+    ws_out = wb.create_sheet(sheet_title)
+    ws_out.append(['国家', '累计确诊', '死亡', '治愈', '现有确诊', '累计确诊增量'])
+    for country in each['subList']:
+        temp_list = [country['country'], country['confirmed'], country['died'], country['crued'], country['curConfirm'], country['confirmedRelative']]
+        for i in range(len(temp_list)):
+            if temp_list[i] == '':
+                temp_list[i] = '0'
+        ws_out.append(temp_list)
+wb.save('./data.xlsx')
+
+
